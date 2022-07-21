1840b4b187f80285e7061efa58f6094dfcb3b52c
stecala <ste.cala123@gmail.com>
2022-07-21 12:43:35 +0200

    🏠 Repo creation

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HeaderApp />
+  </div>
+</template>
+
+<script>
+import HeaderApp from './components/HeaderApp.vue'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderApp,
+  },
+  data : function(){
+    return {
+      api_key : '290ef3b32debba72662776b92b209938',
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+
+</style>
