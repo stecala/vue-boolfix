ec1afa3f7cefee7368f58e67d3cb50639ff8ed22
stecala <ste.cala123@gmail.com>
2022-07-24 11:36:10 +0200

    Feat ⭐ hover on most popular now shows the youtube icon to link the trailer

--- src/components/MostPopularTvsCards.vue
+++ src/components/MostPopularTvsCards.vue
@@ -5,6 +5,13 @@
             <hr class="ms-2">
             <div v-for="element in mostPopularTvsList" :key="element.id" class="img-card-size position-relative">
                 <img :src="completePosterPathW342(element.poster_path)" :alt="element.title">
+                <div class="my-trailer-container position-absolute d-flex align-items-center justify-content-center flex-column">
+                    <div class="txt-trailer">Guarda il trailer</div>
+                    <div class="a-container">
+                        <a :href="linkToYT(element)"><i class="fa-brands fa-youtube"></i>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -25,10 +32,43 @@ export default {
             }
             return path;
         },
+        linkToYT(element){
+            let link = element.original_name.replaceAll(' ','+')
+            return  'https://www.youtube.com/results?search_query='+link+'+trailer'
+        }, 
     },
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+    
+    .my-trailer-container{
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 12px;
+        background-color: rgba(0,0,0,0.7);
+        visibility: hidden;
+        opacity: 0;
+        transition: visibility 0s ease-in 0ms, opacity 0ms;
+        .txt-trailer{
+            font-size: 2rem;
+        }
+        .a-container{
+            a{
+                font-style: none;
+                color: rgb(255,0,0);
+                font-size: 4rem;
+                z-index:3;
+            }
+           
+        }
+        
+    }
+    .img-card-size:hover .my-trailer-container{
+        visibility: visible;
+        opacity: 1;
+        transition: visibility 1s ease-in 0ms, opacity 300ms;
+    }
 
-</style>
+</style>>
